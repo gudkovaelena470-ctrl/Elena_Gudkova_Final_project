b1d24784f16d2526466ef23a1652b28353b2ed54
gudkovaelena470-ctrl <gudkovaelena470@gmail.com>
2026-02-23 18:57:15 +0300

    Обновила репозиторий по замечаниям

--- create_order_get_by_track_test.py
+++ create_order_get_by_track_test.py
@@ -3,18 +3,9 @@ from sender_stand_request import post_new_order, get_order_by_track
 from data import order_body
 
 
-def test_create_order_and_get_by_track():
+def test_get_order_by_track():
     
     create_response = post_new_order(order_body)
-    assert create_response.status_code == 201, f"Create order failed: {create_response.text}"
-
-    create_json = create_response.json()
-    track = create_json.get("track")
-    assert track is not None, f"No 'track' in response: {create_json}"
-
+    track = create_response.json()["track"]
     get_response = get_order_by_track(track)
-
-    assert get_response.status_code == 200, f"Get order by track failed: {get_response.text}"
-
-    get_json = get_response.json()
-    assert "order" in get_json, f"No 'order' in response: {get_json}"
+    assert get_response.status_code == 200
